59a3aeedd060a7c51f02f82d87b7168c731c799a
DynCoder <joshuavwaardenberg@gmail.com>
2023-01-21 18:30:02 +0100

    Separate files

--- src/brainf.h
+++ src/brainf.h
@@ -9,18 +9,4 @@
  *
  */
 
-#include <stdio.h>
-#include <brainf.h>
-
-int main(int argc, char *argv[]) {
-
-    if (argc < 2) {
-        printf("No file passed\n");
-        return 1;
-    }
-
-    bf_run_file(argv[1]);
-
-    return 0;
-
-}
+int bf_run_file(char *filename);
